437a691a1a70422c8f4e70673165b718096a0930
Andrea Santona <andreasantonandreasantona.gi@gmail.com>
2021-10-13 11:15:53 +0200

    Implemented functioning portals

--- modules/entities/portal/portal.js
+++ modules/entities/portal/portal.js
@@ -5,14 +5,21 @@ export class Portal extends Entity {
         super(x, y);
         this.dir = dir;
         this.id = id;
-        this.solid = false;
+        this.solid = true;
         this.type = "portal";
         this.background = true;
         this.spriteW = 3;
         this.spriteH = 1;
-        this.hitbox = false;
+        this.hitbox = {
+            x: this.x,
+            y: this.y,
+            w: this.w,
+            h: this.h
+        };
+        this.left = 0;
 
         this.animation = "open";
+
         this.setAnimation("closed", [15], [19])
         this.setAnimation("opening", [15, 15, 15, 15, 15, 15, 15, 15, 15], [19, 20, 21, 22, 23, 24, 25, 26, 27])
         this.setAnimation("open", [15], [27])
@@ -38,9 +45,9 @@ export class Portal extends Entity {
             }
         }
     }
-    computeAction() {
+    computeAction(entities) {
         let enemiesAlive = false;
-        for (let entity of map.entities) {
+        for (let entity of entities) {
             if (entity.removed) {
                 continue;
             }
@@ -49,38 +56,47 @@ export class Portal extends Entity {
                 break;
             }
         }
-
-        if (this.action == 0 && !enemiesAlive) {
-            this.action = 1;
-        }
-        if (this.action == 2 && enemiesAlive) {
-            this.action = 3;
+        /* 
+                if (this.animation == "closed" && !enemiesAlive) {
+                    this.animation = "opening";
+                }
+                if (this.animation == "open" && enemiesAlive) {
+                    this.animation = "closing";
+                }
+                 */
+        if (this.animation == "open" || this.animation == "opening") {
+            this.solid = false;
+        } else {
+            this.solid = true;
         }
     }
-    compute() {
-        /* 
-
-        if (!this.solid && collided(this, player)) {
+    /** portal computing */
+    computePortal(meta, gameDirector) {
+        //
+        this.computeAction(gameDirector.level.entities);
+        if (!this.solid && this.Physics.collided(this, gameDirector.player)) {
             // Move player to the linked level
             gameDirector.changeLevel(this.dir, meta);
         }
- */
-        //this.computeAction();
     }
+    /** Entity computing */
+
+    compute(deltaTime) {
+        this.updateHitbox();
+        this.updateSprite(deltaTime);
+    }
+
     onAnimationEnd() {
         switch (this.animation) {
             case "opening":
-                this.action = "open";
-                this.solid = false;
+                this.animation = "open";
                 break;
             case "closing":
-                this.action = "closed";
-                this.solid = true;
+                this.animation = "closed";
                 break;
         }
     }
     render(context, tilesize, ratio, camera) {
-        this.updateSprite();
         if (this.removed) {
             // If the entity is removed, don't bother rendering
             return;
