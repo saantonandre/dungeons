555a903efba2a7cb744e75a8503b997054b72ef6
Andrea Santona <andreasantonandreasantona.gi@gmail.com>
2021-10-22 23:04:13 +0200

    commenting a little more

--- modules/entity/sprite.js
+++ modules/entity/sprite.js
@@ -5,9 +5,13 @@ export class Sprite {
         this.y = y;
         this.w = 1;
         this.h = 1;
+        /** Each entity has this same default spritesheet, but it can get rewritten */
         this.sheet = spritesheet;
+        /** The animation currently rendering (defaults to 'idle') */
         this.animation = "idle";
         this.animations = {};
+
+        /** The animation currently rendering (defaults to 'idle') */
         this.display = true;
         this.frame = 0;
         this.left = 0;
@@ -26,8 +30,24 @@ export class Sprite {
         this.animation = label;
         return true;
     }
+    /** 
+     * What happens after the current animation ends (this function gets rewritten by entities classes) 
+     * If no action are specified for the current animation, it will just reiterates
+     */
     onAnimationEnd() {
-        // What happens after the current animation ends
+        /** 
+         *  // Example code inside this function:
+         * 
+         * switch(this.animation){
+         *      case 'death':
+         *          this.removed=true;
+         *          break;
+         *      case 'attack':
+         *          this.loadAnimation('idle');
+         *          break;
+         * }
+         * 
+         */
     }
     /** Renders the base sprite, not animated as a static icon, in a specific location */
     renderItem(x, y, context, tilesize, ratio, w = 1, h = 1) {
@@ -43,18 +63,21 @@ export class Sprite {
             h * tilesize * ratio // height of the entity
         );
     }
+    /** Progresses the sprite animation */
     updateSprite(deltaTime) {
         this.frameCounter += deltaTime;
         if (this.frameCounter >= this.animations[this.animation].slowness) {
             this.frame++;
             this.frameCounter = 0;
         }
+        /** If the current frame exceeded the current animation length */
         if (this.frame >= this.animations[this.animation].keyframesX[this.left].length) {
             this.frame = 0;
             this.frameCounter = 0;
             this.onAnimationEnd();
         }
     }
+    /** Adds or overwrites an animation */
     setAnimation(label, keyframesX, keyframesY, left = 0) {
         if (!this.animations[label]) {
             this.animations[label] = new Animation(this);
@@ -63,6 +86,7 @@ export class Sprite {
             this.animations[label].keyframesX[1] = keyframesX;
             this.animations[label].keyframesY[1] = keyframesY;
         } else {
+            /** If left-facing animation was not specified use the keyframes for both ways */
             this.animations[label].keyframesX[0] = keyframesX;
             this.animations[label].keyframesY[0] = keyframesY;
 
@@ -72,19 +96,18 @@ export class Sprite {
 
     }
     renderSprite(context, tilesize, ratio, camera = { x: 0, y: 0 }, rot = false) {
-
         if (!this.display) {
+            /** Skips rendering */
             return;
         }
 
-        // Offset given by the difference of the animation relative to the actual width/height of the entity
         if (rot) {
+            /** Rotated Rendering */
             context.save();
             context.translate(
                 (this.x + this.animations[this.animation].offsetX + this.w / 2 + camera.x) * tilesize * ratio,
                 (this.y + this.animations[this.animation].offsetY + this.h / 2 + camera.y) * tilesize * ratio
             )
-            //
             context.rotate(rot);
             context.drawImage(
                 this.sheet, // source of the sprite
@@ -99,6 +122,7 @@ export class Sprite {
             );
             context.restore();
         } else {
+            /** Default Rendering */
             context.drawImage(
                 this.sheet, // source of the sprite
                 this.animations[this.animation].keyframesX[this.left][this.frame] * tilesize, // x pos of the sprite
@@ -116,18 +140,23 @@ export class Sprite {
 }
 class Animation {
     constructor(entity) {
+        /** X positions on the spritesheet */
         this.keyframesX = [
-                [],
-                []
-            ],
-            this.keyframesY = [
-                [],
-                []
-            ],
-            this.slowness = 6,
-            this.w = entity.w,
-            this.h = entity.h,
-            this.offsetX = (entity.w - this.w) / 2,
-            this.offsetY = (entity.h - this.h) / 2
+            [],
+            []
+        ];
+        /** Y positions on the spritesheet */
+        this.keyframesY = [
+            [],
+            []
+        ];
+        /** Defines how many game-frames should be skipped before each next animation-frame */
+        this.slowness = 6;
+        // Size standards to the entity size, but can be changed
+        this.w = entity.w;
+        this.h = entity.h;
+        // Some Animations may need an offset relatively to the entity position
+        this.offsetX = (entity.w - this.w) / 2;
+        this.offsetY = (entity.h - this.h) / 2;
     }
 }
